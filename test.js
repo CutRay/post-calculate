88cb9d2973d263d93ddf13e9e17903461deee415
CutRay <zeron9421@gmail.com>
2020-04-14 00:12:41 +0900

    マルチスレッド試してみた

--- test.js
+++ test.js
@@ -18,22 +18,26 @@ rl.on('close', function () {
         const data=el.replace('\t',' ').split(' ')
         const count=Number(data[0])
         const price = Number(data[1])
-        pricePattern(count, price)
+        test(count, price)
         console.log('\n')
     })
     process.exit(0)
 })
-
-function  pricePattern(num, price, para = [], count = 1) {
+function test(count,price) {
+    pricePattern(count, price)
+    console.log(res)
+}
+let res=[]
+async function pricePattern(num, price, para = [], count = 1) {
     for (let i = 0; i <= num; i++){
         para.push(i)
-        if (count !== 6) {
+        if (count !== 5) {
             count += 1
             pricePattern(num - i, price, para, count)
             count -= 1
         }
-        if (count === 6&&i===num&&para[0]*580+para[1]*390+para[2]*250+para[3]*210+para[4]*140+para[5]*120===price) {
-            console.log(para.join('\t')+'\t'+para.reduce((a,x) => a+=x))
+        if (count === 5&&i===num&&para[0]*390+para[1]*250+para[2]*210+para[3]*140+para[4]*120===price) {
+            res.push(para.join('\t'))
         }
         para.pop()
     }
